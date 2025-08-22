cfce9504339d63525702a14a49beda41c68ad8d2
RaviChatta <92024309+RaviChatta@users.noreply.github.com>
2025-08-22 23:58:45 +0530

    Update __init__.py

--- Webs/__init__.py
+++ Webs/__init__.py
@@ -13,4 +13,5 @@ from .manhuaplus import ManhuaplusWebs
 from .mgeko import MgekoWebs
 from .manhwa18 import Manhwa18Webs
 from .manga18fx import Manga18fxWebs
-from .nhentai import NHentaiWebs  # Add this line
+from .nhentai import NHentaiWebs
+from .bato import BatoWebs  # Add this line
